435858981e411fea7ad3a7e58044e66668de1802
Axel Voitier <axel.voitier@gmail.com>
2020-02-01 16:35:03 +0100

    QA: Fixes some linter issues

--- lookups/instance_content.py
+++ lookups/instance_content.py
@@ -4,7 +4,7 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
-from __future__ import annotations  # noqa: T484
+from __future__ import annotations  # noqa: F407
 
 # System imports
 import weakref
